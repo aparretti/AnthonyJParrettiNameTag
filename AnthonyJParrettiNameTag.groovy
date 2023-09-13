8aa013f3d618ed3ddc21f6da0774bc9db171a9c7
aj <aj@localhost>
2023-09-13 11:10:43 -0400

    Nun mutch

--- AnthonyJParrettiNameTag.groovy
+++ AnthonyJParrettiNameTag.groovy
@@ -1,5 +1,15 @@
 // code here
 
+import eu.mihosoft.vrl.v3d.*;
+import javafx.scene.text.Font;
+
+Font font = new Font("Arial",  30);
+
+return TextExtrude.text((double)10.0,"Hello World 1234!",font).collect{
+	it.rotx(180)
+	.toZMin()
+
+}
 double width = 30
 double depth= 70
 double height =3
